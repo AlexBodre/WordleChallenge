3481c99336cedfff5887531ed0f4167de9029277
Alexander Bodre <aboordonez@gmail.com>
2023-02-07 17:03:37 -0400

    Ui test
    Ui test

---
+++ WordleChallenge/WordleUI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace WordleChallenge
+{
+	public class WordleUI
+	{
+
+
+		public void GameScreen(GuessedWord[] guessedWords)
+		{
+			Console.Clear();
+			Console.WriteLine("Welcome to Alex's Wordle");
+
+
+			foreach(GuessedWord guessedWord in guessedWords)
+			{
+				if (string.IsNullOrEmpty(guessedWord.Word))
+				{
+					Console.WriteLine("-----");
+				}
+				else
+				{
+					var line = new StringBuilder();
+					foreach (var guessedLetter in guessedWord.GuessedLetters)
+					{
+						if (true)
+						{
+
+						}
+					}
+				}
+			}
+		}
+	}
+}
+
