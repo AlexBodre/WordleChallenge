d8c287dd18fc07f2e81e194d2c70569a1386f343
Alexander Bodre <aboordonez@gmail.com>
2023-02-07 20:29:14 -0400

    Ui done
    Ui done

--- WordleChallenge/Program.cs
+++ WordleChallenge/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using WordleChallenge;
+
 Console.WriteLine("Hello, World!");
 
+var gameFlowProcess = new GameFlowProcess(WordleChallenge.ListOfWords.list);
+gameFlowProcess.Menu();
