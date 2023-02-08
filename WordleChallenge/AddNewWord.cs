a8b60a2dadceee01204521fdd104b4db296aa9c3
Alexander Bodre <aboordonez@gmail.com>
2023-02-08 17:22:08 -0400

    AddNewWord
    AddNewWord

---
+++ WordleChallenge/AddNewWord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace WordleChallenge
+{
+	public class AddNewWord
+	{
+
+		public static string[] CaptureWord()
+		{
+            Console.WriteLine("Add a new word of 5 letters");
+
+            var list = WordleChallenge.ListOfWords.list;
+            var newElement = Console.ReadLine();
+            List<string> elements = new List<string>(list.ToList());
+			if(!string.IsNullOrEmpty(newElement))
+            elements.Add(newElement);
+
+
+            list = elements.ToArray();
+			return list;
+        }
+
+		public AddNewWord()
+		{
+		}
+	}
+}
+
